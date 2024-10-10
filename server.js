1d3ef028461dd9e03759e631fcc95b7a2d54a964
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-10-09 20:54:24 -0500

    5%

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-import express from "express";
+import express from 'express';
+import "dotenv/config.js"
 
-const server = express()
+const server = express();
+
+const PORT = process.env.PORT || 8080
+
+const ready = () => console.log("Server running in port :"+PORT)
+
+server.get('/',(request,response)=>{
+    response.send('Hola mundo Express')
+})
+server.get('/saludo',(request,response)=>{
+    response.send('Hola Andres Express')
+})
+
+server.listen(PORT,ready)
