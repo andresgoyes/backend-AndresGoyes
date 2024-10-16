ec74562547303334b573097986e7032b33798a45
Andres Goyes <andres.goyes@correounivalle.edu.co>
2024-10-15 20:02:43 -0500

    Se agrego create users

--- router/users.js
+++ router/users.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { allUser,userByRole } from "../controllers/users/read.js"
+import create from "../controllers/users/create.js"
 
 const router = Router()
 
 router.get('/all', allUser)
 router.get('/role/:x', userByRole)
+router.post('/create/', create)
 
     
 export default router
